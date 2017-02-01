7bf08d78a6d347b4404f71b13b72b89b5d395af2
anandpimple <anandpimple@tfl.gov.uk>
2017-02-01 22:35:48 +0000

    Extracted main functionality into Test case and created interface method accordingly.

--- src/main/java/candidate/anandpimple/WebCrowler.java
+++ src/main/java/candidate/anandpimple/WebCrowler.java
@@ -1,7 +1,11 @@
 package candidate.anandpimple;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by anandpimple on 01/02/2017.
  */
 public interface WebCrowler {
+    Map<String,List<String>> visitAndExtract(String s);
 }
--- src/test/java/candidate/anandpimple/WebCrowlerTest.java
+++ src/test/java/candidate/anandpimple/WebCrowlerTest.java
@@ -1,10 +1,20 @@
 package candidate.anandpimple;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Created by anandpimple on 01/02/2017.
  */
 class WebCrowlerTest {
-
+    private WebCrowler crowler;
+    //Main Requirment is to visit and extract page data for required page.
+    @Test
+    public void testVisitAndExtract(){
+        Map<String,List<String>> data = crowler.visitAndExtract("http://wiprodigital.com");
+    }
 }
